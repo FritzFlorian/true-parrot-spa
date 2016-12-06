1f6cfed5d50bd566578ab0acbc1488115afbd1a7
FritzFlorian <codingtipp@gmail.com>
2016-12-06 19:22:17 +0100

    Add flash message to logout

--- src/viewmodels/logout/logout.ts
+++ src/viewmodels/logout/logout.ts
@@ -1,15 +1,20 @@
 import {autoinject} from "aurelia-framework";
 import TwitterCloneService from "../../services/twitterCloneService";
+import {FlashMessage} from "../../services/messages";
+import {EventAggregator} from "aurelia-event-aggregator";
 
 @autoinject()
 export class Logout {
   service: TwitterCloneService;
+  ea: EventAggregator;
 
-  constructor(service:TwitterCloneService) {
+  constructor(service:TwitterCloneService, ea:EventAggregator) {
     this.service = service;
+    this.ea = ea;
   }
 
   logout() {
+    this.ea.publish(new FlashMessage("Logged Out"));
     this.service.logout();
   }
 
