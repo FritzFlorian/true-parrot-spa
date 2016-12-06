4af55afe36365e635b26669a74bcf3ba99a1adb9
FritzFlorian <codingtipp@gmail.com>
2016-12-06 19:28:35 +0100

    Add flash messages to login and signup

--- src/viewmodels/login/login.ts
+++ src/viewmodels/login/login.ts
@@ -2,21 +2,26 @@ import {autoinject} from "aurelia-framework";
 import TwitterCloneService from "../../services/twitterCloneService";
 import {ServiceError} from "../../services/twitterCloneService";
 import User from "../../services/user";
+import {FlashMessage} from "../../services/messages";
+import {EventAggregator} from "aurelia-event-aggregator";
 
 @autoinject()
 export class Login {
   service: TwitterCloneService;
+  ea: EventAggregator;
   email: string;
   password: string;
 
   formErrors: any[];
 
-  constructor(service:TwitterCloneService) {
+  constructor(service:TwitterCloneService, ea:EventAggregator) {
     this.service = service;
+    this.ea = ea;
   }
 
   login(event) {
     this.service.login(this.email, this.password).then((user:User) => {
+      this.ea.publish(new FlashMessage("Logged In"));
     }).catch((error:ServiceError) => {
       this.formErrors = error.formErrors;
     });
--- src/viewmodels/signup/signup.ts
+++ src/viewmodels/signup/signup.ts
@@ -1,10 +1,13 @@
 import {autoinject} from "aurelia-framework";
 import TwitterCloneService from "../../services/twitterCloneService";
 import {ServiceError} from "../../services/twitterCloneService";
+import {FlashMessage} from "../../services/messages";
+import {EventAggregator} from "aurelia-event-aggregator";
 
 @autoinject()
 export class Signup {
   service: TwitterCloneService;
+  ea: EventAggregator;
   email: string;
   password: string;
   firstName: string;
@@ -13,13 +16,14 @@ export class Signup {
 
   formErrors: any[] = [];
 
-  constructor(service:TwitterCloneService) {
+  constructor(service:TwitterCloneService, ea:EventAggregator) {
     this.service = service;
+    this.ea = ea;
   }
 
   register(event) {
     this.service.register(this.email, this.password, this.firstName, this.lastName, this.description).then((result) => {
-
+      this.ea.publish(new FlashMessage("Account Created"));
     }).catch((error:ServiceError) => {
       this.formErrors = error.formErrors;
     });
