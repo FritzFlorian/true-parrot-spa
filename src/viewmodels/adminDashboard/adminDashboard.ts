8df6a0369e794a418f7f5173ebca45521ea180a2
FritzFlorian <codingtipp@gmail.com>
2016-12-07 10:05:41 +0100

    Add latest tweets to admin dashboard

--- src/viewmodels/adminDashboard/adminDashboard.ts
+++ src/viewmodels/adminDashboard/adminDashboard.ts
@@ -1,3 +1,34 @@
+import Tweet from "../../services/tweet";
+import {autoinject} from "aurelia-framework";
+import TwitterCloneService from "../../services/twitterCloneService";
+import {EventAggregator} from "aurelia-event-aggregator";
+import {TweetsChanged} from "../../services/messages";
+import User from "../../services/user";
+
+@autoinject()
 export class AdminDashboard {
+  service: TwitterCloneService;
+  tweets: Tweet[];
+  currentUser: User;
+
+  constructor(service:TwitterCloneService, ea:EventAggregator) {
+    this.service = service;
+    this.currentUser = service.currentUser;
+
+    ea.subscribe(TweetsChanged, (message:TweetsChanged) => {
+      this.setTweets(message.tweets);
+    });
+    this.service.reloadTweets();
+  }
+
+  setTweets(tweets:Tweet[]) {
+    this.tweets = tweets.slice(0, 5);
+  }
+
+  attached() {
+    this.setTweets(this.service.tweets);
+
+    runJquery();
+  }
 
 }
